73a5ccbe729f4fe3c47f2ee27421bfc93352e010
Mubashir01234 <omerkhan01234@gmail.com>
2023-05-11 22:10:24 +0500

    add cors

--- go/cmd/main.go
+++ go/cmd/main.go
@@ -19,5 +19,20 @@ func init() {
 
 func main() {
 	r := gin.New()
+	r.Use(enableCors())
 	log.Fatal(http.ListenAndServe(":"+config.Cfg.ServerPort, server.NewServerImpl(r)))
+}
+
+func enableCors() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	}
 }
